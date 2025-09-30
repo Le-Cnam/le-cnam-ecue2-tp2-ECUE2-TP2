d36224a243f5269ba4dfb1bce637c464b4b0d2c8
Maxime <maxime@eliobot.com>
2025-09-30 11:11:00 +0200

    feat(exercice 11) fait

--- src/net/lecnam/ussi47/tp2/Exercice11.java
+++ src/net/lecnam/ussi47/tp2/Exercice11.java
@@ -4,6 +4,10 @@ public class Exercice11 {
 
     public static void main(String[] args) {
 
+        String uneChaine = "CNAM";
 
+        String uneChaineModifiee = uneChaine.charAt(0) + uneChaine.substring(1).toLowerCase();
+
+        System.out.println(uneChaineModifiee);
     }
 }
