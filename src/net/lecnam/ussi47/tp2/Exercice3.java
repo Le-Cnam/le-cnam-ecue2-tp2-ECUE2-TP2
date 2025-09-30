437be43b598d4cf9ed3c7fcbdda9316dfd75b6a7
Cesar Pierrard <cesar.pierrard.auditeur@lecnam.net>
2025-09-30 11:57:13 +0200

    Exercice 3

--- src/net/lecnam/ussi47/tp2/Exercice3.java
+++ src/net/lecnam/ussi47/tp2/Exercice3.java
@@ -3,7 +3,15 @@ package net.lecnam.ussi47.tp2;
 public class Exercice3 {
 
     public static void main(String[] args) {
-
-
+        float cote = 1.25f;
+        float surface = cote*cote;
+        byte nbJardin = 4;
+        byte plantes = 9;
+        float surfaceTot = surface * nbJardin;
+        System.out.println("la surface tot = " + surfaceTot);
+        int plantesTot = plantes * nbJardin;
+        System.out.println("le nombre de plantes tot = " + plantesTot);
+        float surfaceParPlante = surfaceTot/plantesTot;
+        System.out.println("la surface parte tot = " + surfaceParPlante);
     }
 }
