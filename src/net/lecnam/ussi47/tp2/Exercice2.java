894dec0c3566e06a37f1fa4b680b9f310ce738cf
Florent Fontaine <florent.fontaine150@gmail.com>
2025-09-30 10:44:08 +0200

    Exercice 2

--- src/net/lecnam/ussi47/tp2/Exercice2.java
+++ src/net/lecnam/ussi47/tp2/Exercice2.java
@@ -3,7 +3,16 @@ package net.lecnam.ussi47.tp2;
 public class Exercice2 {
 
     public static void main(String[] args) {
+        byte a = 3;
+        byte b = 4;
+        byte c = 5;
 
+        int aCarre = a * a;
+        int bCarre = b * b;
+        int cCarre = c * c;
 
+        boolean res = (aCarre + bCarre == cCarre);
+
+        System.out.println("Résultat : " + res);
     }
 }
