afbb59b9908e90cdece90417f35ee13d79e063a8
Cesar Pierrard <cesar.pierrard.auditeur@lecnam.net>
2025-09-30 11:23:03 +0200

    Exercice 1

--- src/net/lecnam/ussi47/tp2/Exercice1.java
+++ src/net/lecnam/ussi47/tp2/Exercice1.java
@@ -3,6 +3,25 @@ package net.lecnam.ussi47.tp2;
 public class Exercice1 {
 
     public static void main(String[] args) {
+        byte i = 12;
+        System.out.println(i);
+        float f = 2.26f;
+        System.out.println(f);
+        short j = -300;
+        System.out.println(j);
+        long l = 3000000000L;
+        System.out.println(l);
+        int k = 2000000000;
+        System.out.println(k);
+        char c = 'a';
+        System.out.println(c);
+        String s = "Hello";
+        System.out.println(s);
+        byte p = 1;
+        System.out.println(p);
+        int q = 29000;
+        System.out.println(q);
+
 
 
     }
