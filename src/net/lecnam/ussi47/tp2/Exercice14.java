3da79f9d9b2d1b43dd1eca18676c574dc3e4722b
Maxime <maxime@eliobot.com>
2025-09-30 11:30:44 +0200

    feat(exercice 14) fait

--- src/net/lecnam/ussi47/tp2/Exercice14.java
+++ src/net/lecnam/ussi47/tp2/Exercice14.java
@@ -4,6 +4,21 @@ public class Exercice14 {
 
     public static void main(String[] args) {
 
+        String unPalindrome = "Esope reste ici et se repose";
 
+        // on supprime les espaces et on met en minuscule
+        String unPalindromeModifie = unPalindrome.replace(" ", "").toLowerCase();
+
+        String unPalindromeInverse = "";
+
+        for (int i = unPalindromeModifie.length() - 1; i >= 0; i--) {
+            unPalindromeInverse += unPalindromeModifie.charAt(i);
+        }
+
+        if (unPalindromeModifie.equals(unPalindromeInverse)) {
+            System.out.println("La chaine est un palindrome");
+        } else {
+            System.out.println("La chaine n'est pas un palindrome");
+        }
     }
 }
