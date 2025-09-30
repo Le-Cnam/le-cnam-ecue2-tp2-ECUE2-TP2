2b84c7e12c4bf869962cfe778cd54f0170ad7121
Florent Fontaine <florent.fontaine150@gmail.com>
2025-09-30 11:58:02 +0200

    Exercice 8

--- src/net/lecnam/ussi47/tp2/Exercice7.java
+++ src/net/lecnam/ussi47/tp2/Exercice7.java
@@ -5,11 +5,8 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Exercice7 {
 
     public static void main(String[] args) {
-        int te = ThreadLocalRandom.current().nextInt(1900,2050);
-        System.out.println(te);
-        System.out.println(te %4 == 0);
-        System.out.println(te % 100 != 0);
-        System.out.println(te % 400 == 0);
-        System.out.println((te %4 == 0 && te % 100 != 0) || te % 400 == 0);
+        int x = ThreadLocalRandom.current().nextInt(1, 101);
+        System.out.println("Le nombre " + x + " est " + (x % 2 == 0 ? "Pair" : "Impair"));
+
     }
 }
