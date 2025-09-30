bea7367b829fcdcaf504c7172d100085d4ed6a43
Florent Fontaine <florent.fontaine150@gmail.com>
2025-09-30 11:23:42 +0200

    Exercice 4

--- src/net/lecnam/ussi47/tp2/Exercice4.java
+++ src/net/lecnam/ussi47/tp2/Exercice4.java
@@ -3,7 +3,9 @@ package net.lecnam.ussi47.tp2;
 public class Exercice4 {
 
     public static void main(String[] args) {
-
+        float pied = 3.2808399f;
+        int hauteur = 4000;
+        System.out.println("Hauteur en pieds :" + pied*hauteur);
 
     }
 }
