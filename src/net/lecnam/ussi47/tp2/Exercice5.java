3e30785a81670ad7dcb101a5d117a1011a609b3e
Florent Fontaine <florent.fontaine150@gmail.com>
2025-09-30 11:37:40 +0200

    Exercice 5

--- src/net/lecnam/ussi47/tp2/Exercice5.java
+++ src/net/lecnam/ussi47/tp2/Exercice5.java
@@ -3,7 +3,10 @@ package net.lecnam.ussi47.tp2;
 public class Exercice5 {
 
     public static void main(String[] args) {
-
+        float volume = 3.40f * 4.30f * 2.30f;
+        System.out.println(volume);
+        float nbRadiateurs = (volume / 8);
+        System.out.println(Math.ceil(nbRadiateurs));
 
     }
 }
