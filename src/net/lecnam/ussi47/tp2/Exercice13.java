bc34abe94f00a438d4f544a39713f6cb46a91efa
Florent Fontaine <florent.fontaine150@gmail.com>
2025-09-30 12:21:43 +0200

    Exercice 13

--- src/net/lecnam/ussi47/tp2/Exercice13.java
+++ src/net/lecnam/ussi47/tp2/Exercice13.java
@@ -1,9 +1,19 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice13 {
 
     public static void main(String[] args) {
-
+        int chiffre = ThreadLocalRandom.current().nextInt(0,9);
+        System.out.println(chiffre);
+        int i = 0;
+        int res = 1;
+        while(i < chiffre){
+            res = res*(i+1);
+            i++;
+        }
+        System.out.println(res);
 
     }
 }
