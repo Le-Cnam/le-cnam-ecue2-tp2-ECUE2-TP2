9570fe7fb422301041e2f8b356e21d8c6bffa18a
Maxime <maxime@eliobot.com>
2025-09-30 10:41:34 +0200

    feat(exercice 6) fait

--- src/net/lecnam/ussi47/tp2/Exercice6.java
+++ src/net/lecnam/ussi47/tp2/Exercice6.java
@@ -4,6 +4,13 @@ public class Exercice6 {
 
     public static void main(String[] args) {
 
+        // Sachant que ThreadLocalRandom.current().nextInt(0,2) retourne 1 ou 0,
+        // écrire un programme qui affiche true si 1 ou false si 0 selon le résultat retourné.
+
+        int randomValue = java.util.concurrent.ThreadLocalRandom.current().nextInt(0, 2);
+        boolean result = (randomValue == 1);
+        System.out.println("Random Value: " + randomValue);
+        System.out.println(result);
 
     }
 }
