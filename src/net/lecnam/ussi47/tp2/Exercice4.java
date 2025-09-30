86ccc0fe140c493ce2829a9e519947d49b024e3f
Maxime <maxime@eliobot.com>
2025-09-30 10:33:34 +0200

    feat(exercice 4) fait

--- src/net/lecnam/ussi47/tp2/Exercice4.java
+++ src/net/lecnam/ussi47/tp2/Exercice4.java
@@ -4,6 +4,15 @@ public class Exercice4 {
 
     public static void main(String[] args) {
 
+        // Les parachutistes sautent souvent à 4000 mètres.
+        // Afficher le facture de conversion et calculer et afficher la hauteur en "pieds" ?
+
+        int metres = 4000;
+        float conversionMetresEnPieds = 3.28084F;
+        float pieds = metres * conversionMetresEnPieds;
+
+        System.out.println(metres + " mètres = " + pieds + " pieds");
+
 
     }
 }
