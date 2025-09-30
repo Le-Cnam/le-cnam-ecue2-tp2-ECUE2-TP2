d183c6cb49a0b288072d01d0756d952296a69a30
Maxime <maxime@eliobot.com>
2025-09-30 11:52:19 +0200

    feat(exercice 16) fait

--- src/net/lecnam/ussi47/tp2/Exercice16.java
+++ src/net/lecnam/ussi47/tp2/Exercice16.java
@@ -4,6 +4,24 @@ public class Exercice16 {
 
     public static void main(String[] args) {
 
+        int hauteurSapin = 5;
+
+        // i pour les lignes
+        // j pour les colonnes
+
+        for (int i = 0; i <= hauteurSapin; i++) {
+            for (int j = 1; j <= (hauteurSapin - i); j++) {
+                System.out.print(' ');
+            }
+            for (int j = 1; j <= (2 * i + 1); j++) {
+                if (j == 1 || j == (2 * i + 1) || i == hauteurSapin) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
 
     }
 }
