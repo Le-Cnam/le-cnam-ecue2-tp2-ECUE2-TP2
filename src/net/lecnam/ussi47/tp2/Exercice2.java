f2da771e7832fe39e504ad8d8107a9b90a4e1688
Maxime <maxime@eliobot.com>
2025-09-30 10:31:02 +0200

    feat(exercice 2) fait

--- src/net/lecnam/ussi47/tp2/Exercice2.java
+++ src/net/lecnam/ussi47/tp2/Exercice2.java
@@ -4,6 +4,26 @@ public class Exercice2 {
 
     public static void main(String[] args) {
 
+        // Pythagore avec 3, 4 et 5
+
+        int sideA = 3;
+        int sideB = 4;
+        int sideC = 5;
+
+        // aˆ2 + bˆ2 = cˆ2
+
+        int aCarre = sideA * sideA;
+        int bCarre = sideB * sideB;
+        int cCarre = sideC * sideC;
+
+        int result = aCarre + bCarre;
+
+        if (result == cCarre) {
+            System.out.println("Le triangle est rectangle");
+        } else {
+            System.out.println("Le triangle n'est pas rectangle");
+        }
+
 
     }
 }
