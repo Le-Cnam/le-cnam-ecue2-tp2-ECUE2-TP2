3ea1a3c40d5f7ae30f10c09e8d7852a247ad604e
Maxime <maxime@eliobot.com>
2025-09-30 11:42:20 +0200

    feat(exercice 15) fait

--- src/net/lecnam/ussi47/tp2/Exercice15.java
+++ src/net/lecnam/ussi47/tp2/Exercice15.java
@@ -4,6 +4,23 @@ public class Exercice15 {
 
     public static void main(String[] args) {
 
+        int hauteurSapin = 11;
 
+        /*
+        for i in range(taille):
+        espaces = ' ' * (taille - i - 1)
+        etoiles = '*' * (2 * i + 1)
+        print(espaces + etoiles)
+         */
+
+        for (int i = 0; i <= hauteurSapin; i++) {
+            for (int j = 1; j <= (hauteurSapin - i) ; j++) {
+                System.out.print(' ');
+            }
+            for (int j = 1; j <= (2 * i + 1); j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
     }
 }
