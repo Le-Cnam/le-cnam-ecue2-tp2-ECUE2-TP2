f92a1e30d321c83eea9a0e0ee4b8324437bad03e
Camille Menuet <camille.menuet.auditeur@lecnam.net>
2025-09-30 12:06:40 +0200

    Modif Exercice 7

--- src/net/lecnam/ussi47/tp2/Exercice7.java
+++ src/net/lecnam/ussi47/tp2/Exercice7.java
@@ -1,9 +1,18 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice7 {
 
     public static void main(String[] args) {
-
-
+        //Sachant que ThreadLocalRandom.current().nextInt(1,101)
+        //retourne un chiffre entre 1 et 100, écrire un programme qui
+        int random = ThreadLocalRandom.current().nextInt(1,101);
+        if (random % 2 == 0) {
+            System.out.println(random + " est pair");
+        }
+        else  {
+            System.out.println(random + " est impair");
+        }
     }
 }
