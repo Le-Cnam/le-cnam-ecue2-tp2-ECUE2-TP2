bff4c34f0b35d4eae990f2370a6750af4baecd80
Camille Menuet <camille.menuet.auditeur@lecnam.net>
2025-09-30 12:22:10 +0200

    Modif Exercice 9

--- src/net/lecnam/ussi47/tp2/Exercice9.java
+++ src/net/lecnam/ussi47/tp2/Exercice9.java
@@ -3,7 +3,10 @@ package net.lecnam.ussi47.tp2;
 public class Exercice9 {
 
     public static void main(String[] args) {
-
-
+        //Concatenation chaine/nombre : Afficher une chaine contenant
+        //"3896,86 / 4,869 = " complétée par le résultat du calcul
+        float nombre1 = 3896.86f ;
+        float nombre2 = 4.869f ;
+        System.out.println(nombre1 + " / " + nombre2 + " = " + nombre1 / nombre2);
     }
 }
