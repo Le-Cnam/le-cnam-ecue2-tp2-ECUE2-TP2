7462773393f257919f8b851bd6c7363253f2eb01
LARRE Vincent <vincent.larre.auditeur@lecnam.net>
2025-09-30 12:29:32 +0200

    Exercice 5

--- src/net/lecnam/ussi47/tp2/Exercice5.java
+++ src/net/lecnam/ussi47/tp2/Exercice5.java
@@ -3,7 +3,15 @@ package net.lecnam.ussi47.tp2;
 public class Exercice5 {
 
     public static void main(String[] args) {
-
+        double volumePiece = 3.40*4.30*2.30;
+        byte surfaceRadiateur = 8;
+        double nbRadiateur = volumePiece/surfaceRadiateur;
+        int nbRadiateurEntier = (int) nbRadiateur;
+        if (nbRadiateurEntier == nbRadiateur) {
+            System.out.println("Nombre de radiateurs nécessaires: " + nbRadiateur);
+        }
+        else
+            System.out.println("Nombre de radiateurs nécessaires: " + (nbRadiateurEntier+1));
 
     }
 }
