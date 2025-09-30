16cd6d20b1f5844d601f7e8d326227e072c3ceb2
Cesar Pierrard <cesar.pierrard.auditeur@lecnam.net>
2025-09-30 11:39:16 +0200

    Exercice 2

--- src/net/lecnam/ussi47/tp2/Exercice2.java
+++ src/net/lecnam/ussi47/tp2/Exercice2.java
@@ -3,7 +3,15 @@ package net.lecnam.ussi47.tp2;
 public class Exercice2 {
 
     public static void main(String[] args) {
-
+        byte coteA = 3;
+        byte coteB = 4;
+        byte coteC = 5;
+        if (coteA*coteA + coteB*coteB == coteC*coteC) {
+            System.out.println("Ce triangle est rectangle");
+        }
+        else {
+            System.out.println("Ce triangle n est pas rectangle");
+        }
 
     }
 }
