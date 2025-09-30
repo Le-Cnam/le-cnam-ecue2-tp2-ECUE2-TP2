5a3a6c98418fbcaa74b4a8917731cbe041696466
Maxime <maxime@eliobot.com>
2025-09-30 10:59:01 +0200

    feat(exercice 9) fait

--- src/net/lecnam/ussi47/tp2/Exercice9.java
+++ src/net/lecnam/ussi47/tp2/Exercice9.java
@@ -4,6 +4,15 @@ public class Exercice9 {
 
     public static void main(String[] args) {
 
+        // Concatenation chaine/nombre : Afficher une chaine contenant
+        // "3896,86 / 4,869 = " complétée par le résultat du calcul
+
+        double valeurA = 3896.86;
+        double valeurB = 4.869;
+
+        double resultat = valeurA / valeurB;
+
+        System.out.println(valeurA + " / " + valeurB + " = " + resultat);
 
     }
 }
