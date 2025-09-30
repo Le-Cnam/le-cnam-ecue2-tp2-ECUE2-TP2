00dbb26d54d614ba889832552595b68ea03b1f97
Florent Fontaine <florent.fontaine150@gmail.com>
2025-09-30 12:04:12 +0200

    Exercice 11

--- src/net/lecnam/ussi47/tp2/Exercice11.java
+++ src/net/lecnam/ussi47/tp2/Exercice11.java
@@ -3,7 +3,8 @@ package net.lecnam.ussi47.tp2;
 public class Exercice11 {
 
     public static void main(String[] args) {
-
-
+        String texte = "CNAM";
+        String resultat = texte.charAt(0) + texte.substring(1).toLowerCase();
+        System.out.println(resultat);
     }
 }
