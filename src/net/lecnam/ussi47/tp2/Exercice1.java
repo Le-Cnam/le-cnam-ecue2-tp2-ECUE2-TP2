e5047d48ca9c33b9ab965245cdae65404b22ec11
Maxime <maxime@eliobot.com>
2025-09-30 10:30:45 +0200

    feat(exercice 1) fait

--- src/net/lecnam/ussi47/tp2/Exercice1.java
+++ src/net/lecnam/ussi47/tp2/Exercice1.java
@@ -4,6 +4,22 @@ public class Exercice1 {
 
     public static void main(String[] args) {
 
+        short uneDizaine = 12;
 
+        float unDecimal = 2.26F;
+
+        int unEntierNegatif = -300;
+
+        long unGrandEntier = 3_000_000_000L;
+
+        int unGrandEntierUnPeuMoinsGrand = 2000000000;
+
+        char uneLettre = 'a';
+
+        String uneChaineDeCaracteres = "Hello";
+
+        short unEntier = 1;
+
+        short unAutreEntier = 29000;
     }
 }
