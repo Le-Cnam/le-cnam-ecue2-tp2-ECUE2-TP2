8ba03494c1096e98621f7d86a854b273d584c2ea
Elias Farhat <elias.farhat.auditeur@lecnam.net>
2025-09-30 12:24:25 +0200

    Exercice 9

--- src/net/lecnam/ussi47/tp2/Exercice9.java
+++ src/net/lecnam/ussi47/tp2/Exercice9.java
@@ -4,6 +4,10 @@ public class Exercice9 {
 
     public static void main(String[] args) {
 
-
+      String chaine = "3896,86 / 4,869 = ";
+      float nb1 = 3896.86f;
+      float nb2 = 4.869f;
+      float resultat = nb1*nb2;
+      System.out.println(chaine + resultat);
     }
 }
