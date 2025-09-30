4d24242f88a3807daac9af47f36cf730bea28e0a
Camille Menuet <camille.menuet.auditeur@lecnam.net>
2025-09-30 13:43:05 +0200

    Modif Exercice 10

--- src/net/lecnam/ussi47/tp2/Exercice10.java
+++ src/net/lecnam/ussi47/tp2/Exercice10.java
@@ -3,7 +3,10 @@ package net.lecnam.ussi47.tp2;
 public class Exercice10 {
 
     public static void main(String[] args) {
-
-
+        //Convertir la chaine HELLO WORLD en minuscule et hello world en majuscule
+        //et affichez les. Pensez à la richesse de l'API Java !
+        String hw = "Hello World";
+        System.out.println(hw.toUpperCase());
+        System.out.println(hw.toLowerCase());
     }
 }
