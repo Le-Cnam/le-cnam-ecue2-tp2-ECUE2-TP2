07925c8f85efeb140e9cc8d47c12a69f98c7c2ef
LARRE Vincent <vincent.larre.auditeur@lecnam.net>
2025-10-03 15:44:58 +0200

    Exercice 9

--- src/net/lecnam/ussi47/tp2/Exercice9.java
+++ src/net/lecnam/ussi47/tp2/Exercice9.java
@@ -3,6 +3,10 @@ package net.lecnam.ussi47.tp2;
 public class Exercice9 {
 
     public static void main(String[] args) {
+    float x = 3896.86f;
+    float y = 4.869f;
+    float z = x/y;
+    System.out.println(x+"/"+y+"="+z);
 
 
     }
