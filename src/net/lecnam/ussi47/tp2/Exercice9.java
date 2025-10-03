904c150b04738a38f4a621d1f144237490b1e30c
Cesar Pierrard <cesar.pierrard.auditeur@lecnam.net>
2025-10-03 15:39:28 +0200

    Exercice 9

--- src/net/lecnam/ussi47/tp2/Exercice9.java
+++ src/net/lecnam/ussi47/tp2/Exercice9.java
@@ -3,7 +3,9 @@ package net.lecnam.ussi47.tp2;
 public class Exercice9 {
 
     public static void main(String[] args) {
-
+        double nombre1 = 3896.86;
+        double nombre2 = 4.869;
+        System.out.println(nombre1+"/"+nombre2 +" = "+nombre1/nombre2);
 
     }
 }
