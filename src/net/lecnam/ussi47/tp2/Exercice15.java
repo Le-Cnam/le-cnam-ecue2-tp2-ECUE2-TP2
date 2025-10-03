aee2ff46dfa254c8ed47af33c20b8c26eb36c88c
Camille Menuet <camille.menuet.auditeur@lecnam.net>
2025-10-03 10:51:55 +0200

    Modif Exercice 15

--- src/net/lecnam/ussi47/tp2/Exercice15.java
+++ src/net/lecnam/ussi47/tp2/Exercice15.java
@@ -3,7 +3,16 @@ package net.lecnam.ussi47.tp2;
 public class Exercice15 {
 
     public static void main(String[] args) {
-
+        int x = 5;
+        int largeur = 2 * x - 1;
+        for (int i = 1; i <= x; i++) {
+            int etoileNb = 2 * i - 1;
+            String sapin = "*".repeat(etoileNb);
+            int padding = largeur - sapin.length();
+            int padLeft = padding / 2;
+            int padRight = padding - padLeft;
+            System.out.println(" ".repeat(padLeft) + sapin + " ".repeat(padRight));
+        }
 
     }
 }
