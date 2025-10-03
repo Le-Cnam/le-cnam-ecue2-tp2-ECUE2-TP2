f5a39c2290fbd7c342055a47a04754def822ce12
Camille Menuet <camille.menuet.auditeur@lecnam.net>
2025-10-03 10:02:25 +0200

    Modif Exercice 13

--- src/net/lecnam/ussi47/tp2/Exercice13.java
+++ src/net/lecnam/ussi47/tp2/Exercice13.java
@@ -1,9 +1,17 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice13 {
 
     public static void main(String[] args) {
-
-
+        //Calculer le factoriel de ThreadLocalRandom.current().nextInt(0,9)
+        int random = ThreadLocalRandom.current().nextInt(0,9);
+        int fact = 1 ;
+        for ( int i = random; i > 0; i--) {
+            fact *= i ;
+            System.out.println(i + " * " + fact/i + " = " +  fact);
+        }
+        System.out.println("\nFactoriel de " + random + " = " + fact);
     }
 }
