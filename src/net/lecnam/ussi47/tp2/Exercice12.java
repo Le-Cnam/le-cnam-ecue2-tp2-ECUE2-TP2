2f722721c1b4837ace7fbe22ba5172a77b437d3c
Camille Menuet <camille.menuet.auditeur@lecnam.net>
2025-10-03 09:52:42 +0200

    Modif Exercice 12

--- src/net/lecnam/ussi47/tp2/Exercice12.java
+++ src/net/lecnam/ussi47/tp2/Exercice12.java
@@ -3,7 +3,14 @@ package net.lecnam.ussi47.tp2;
 public class Exercice12 {
 
     public static void main(String[] args) {
-
-
+        String phrase = "cette phrase contient 11 voyelles" ;
+        String voyelle = "aeiouy" ;
+        int compteur = 0 ;
+        for (int i = 0; i < phrase.length(); i++) {
+            if (voyelle.contains(phrase.substring(i,i+1))){
+                compteur++;
+            }
+        }
+        System.out.println("il y a : " + compteur + " voyelles");
     }
 }
