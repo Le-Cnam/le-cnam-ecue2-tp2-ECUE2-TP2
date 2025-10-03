9c19bfe978a60e0ef183588fc8b8c867bd758834
Cesar Pierrard <cesar.pierrard.auditeur@lecnam.net>
2025-10-03 15:26:34 +0200

    Exercice 7

--- src/net/lecnam/ussi47/tp2/Exercice7.java
+++ src/net/lecnam/ussi47/tp2/Exercice7.java
@@ -1,9 +1,17 @@
 package net.lecnam.ussi47.tp2;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Exercice7 {
 
     public static void main(String[] args) {
-
+        int resultat = ThreadLocalRandom.current().nextInt(1, 101);
+        System.out.println(resultat);
+        if (resultat % 2 ==0) {
+            System.out.println("Le nombre " +resultat +" est Pair");
+        }
+        else {
+            System.out.println("Le nombre " +resultat +" est impair");
+        }
 
     }
 }
