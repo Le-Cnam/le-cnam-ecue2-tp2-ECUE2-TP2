8daf7e7b3affdbc517cb5af413899d02bd9df1ab
LARRE Vincent <vincent.larre.auditeur@lecnam.net>
2025-10-03 15:29:19 +0200

    Exercice 7

--- src/net/lecnam/ussi47/tp2/Exercice7.java
+++ src/net/lecnam/ussi47/tp2/Exercice7.java
@@ -1,9 +1,15 @@
 package net.lecnam.ussi47.tp2;
-
+import java.util.concurrent.ThreadLocalRandom;
 public class Exercice7 {
 
     public static void main(String[] args) {
-
-
+        int x = ThreadLocalRandom.current().nextInt(1,101);
+        System.out.println(x);
+        if (x%2==0){
+            System.out.println("c'est pair");
+        }
+        else {
+            System.out.println("c'est impair");
+        }
     }
 }
