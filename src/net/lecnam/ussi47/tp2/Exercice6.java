94fda67a72434e6854ba8c88b07fb7176dd7d611
LARRE Vincent <vincent.larre.auditeur@lecnam.net>
2025-10-03 15:24:29 +0200

    Exercice 6

--- src/net/lecnam/ussi47/tp2/Exercice6.java
+++ src/net/lecnam/ussi47/tp2/Exercice6.java
@@ -1,9 +1,19 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice6 {
 
     public static void main(String[] args) {
-
+        int x = ThreadLocalRandom.current().nextInt(0, 2);
+        if (x==0)
+            {
+            System.out.println ("false");
+            }
+        else
+            {
+            System.out.println ("true");
+            }
 
     }
 }
